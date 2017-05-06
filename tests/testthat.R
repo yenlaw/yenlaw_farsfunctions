e236f98ca623a3a1a58447f32db5e6aa96ee82c0
yenlaw <yenlaw@zoho.com>
2017-05-06 18:33:09 +0100

    test trigger

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,4 +6,5 @@ library(farsfunctions)
 
 test_that("str_length of missing is missing", {
   expect_warning(fars_read_years(2017), "invalid year: 2017")
+  expect_warning(fars_read_years(2017), "invalid year: 2017")
 })
